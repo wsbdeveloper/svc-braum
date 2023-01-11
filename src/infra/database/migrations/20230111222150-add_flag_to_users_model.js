f27bbc13a78ee44319e01f5ecb2753b2d0b72a40
ScotuzziJr <scotuzzijunior@gmail.com>
2023-01-11 19:33:26 -0300

    feat: setando flag de primemiro acesso do usuário no banco

---
+++ src/infra/database/migrations/20230111222150-add_flag_to_users_model.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all(
+      queryInterface.addColumn(
+        'Users', // table name
+        'isFirstAccess', // new field name
+        {
+          type: Sequelize.BOOLEAN,
+          allowNull: true,
+        },
+      ))
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
